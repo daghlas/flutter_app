da763864afebe08fcafa16995ad0dc32d9e6712e
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-01-10 13:34:49 +0300

    initial commit - colors and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,23 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('my first flutter app'),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text('hello daghlas world'),
+      child: Text(
+        'Hello world, welcome Daghlas',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {  },
+      backgroundColor: Colors.red[600],
       child: Text('click'),
     ),
   ),
