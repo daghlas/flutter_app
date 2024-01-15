67ab2729cbbb2151eb149cceab5f628506aa2d9e
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-01-15 14:42:37 +0300

    initial commit - containers & padding; EdgeInsets.symmetric

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class Home extends StatelessWidget {
       ),
       body: Container(
         color: Colors.grey[600],
-        padding: EdgeInsets.all(20.0),
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
         child: Text('Hello'),
       ),
       floatingActionButton: FloatingActionButton(
